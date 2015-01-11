b0ecd5572931728805782b8d51697f655d673c29
David Chelimsky <dchelimsky@gmail.com>
2015-01-11 11:10:24 -0200

    Integer exemplars use longs where possible.
    Was generating Long/MAX_VALUE as a BigInt instead of a long

--- src/exemplar.clj
+++ src/exemplar.clj
@@ -12,7 +12,9 @@
 
 (defn range-centered-on
   ([n] (range-centered-on n 5))
-  ([n m] (vec (range (- n m) (+ n m 1)))))
+  ([n m] (mapv
+          #(try (long %) (catch Exception e %))
+          (range (- n m) (+ n m 1)))))
 
 (defn vmap [f s]
   (vec (map f s)))
